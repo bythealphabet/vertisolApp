4d3fd44756e9b452e7ba7ae60b160781f75448c7
bythealphabet <bythealphabet@gmail.com>
2019-01-10 23:22:00 -0400

    messing around with the code trying to use Bulma

---
+++ src/components/Header/NavBar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Navbar } from "react-bulma-components/full";
+
+
+
+  const NavBar = () =>{
+    return(
+    	<div>
+	    	<Navbar color = "danger"></Navbar>
+		</div>
+      )
+  } 
+
+
+export default NavBar
